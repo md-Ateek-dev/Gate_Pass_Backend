d03f661d8071988b539adacbd1dbc751204f21cc
md-Ateek-dev <mohdateek.dev@gmail.com>
2026-05-23 01:05:22 +0530

    add in feature

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { authUser, getUserProfile } from '../controllers/authController.js';
+import { authUser, getUserProfile, changePassword } from '../controllers/authController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile);
+router.put('/password', protect, changePassword);
 
 export default router;
