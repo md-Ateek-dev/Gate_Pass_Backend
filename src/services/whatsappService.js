7a56eadb28e799a4faa7b8b0985636e7c00029e8
md-Ateek-dev <mohdateek.dev@gmail.com>
2026-06-25 23:36:32 +0530

    some changes

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -1,14 +1,24 @@
 import axios from "axios";
 
 export const sendWhatsAppMessage = async (to, message) => {
+  
   try {
+    
+    console.log("📱 Original Number:", to);
+    console.log("📱 Final Number:", `91${to}`);
+    console.log("📱 Phone Number ID:", process.env.WHATSAPP_PHONE_NUMBER_ID);
+    console.log(
+      "📱 Access Token:",
+      process.env.WHATSAPP_ACCESS_TOKEN?.substring(0, 20) + "..."
+    );
+    
     const url = `https://graph.facebook.com/v25.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`;
 
     const response = await axios.post(
       url,
       {
         messaging_product: "whatsapp",
-        to: `91${to}`, // भारत के नंबर के लिए
+        to: `91${to}`,
         type: "text",
         text: {
           body: message,
